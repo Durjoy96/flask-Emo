3d999cfd15a07760734d0c9faa5d792e39a2162a
Durjoy96 <88230012+Durjoy96@users.noreply.github.com>
2023-07-29 17:27:42 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,50 +1,7 @@
-'''from flask import Flask, render_template, request
-import openai
-
-openai.api_key = "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"
-
-app = Flask(__name__)
-app.config['TEMPLATES_AUTO_RELOAD'] = True
-app.static_folder = 'static'
-
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-@app.route('/generate', methods=['POST'])
-def generate():
-    question = request.form['text']
-
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {
-                "role": "system",
-                "content": "You are an emoji expert who knows all the existing emojis.user will inputted sentences and you will and you will  add emojis without changing any word or sentences.Your output must obey the following constraints:1. Don't change or replace word with emojis 2. Always add the correct emoji 3. Whenever an object catches your attention, add an emoji if you find an appropriate one 4. Only do what I instructed you to do"
-            },
-            {
-                "role": "user",
-                "content": question
-            }
-        ],
-        temperature=1,
-        max_tokens=2000,
-        top_p=1,
-        frequency_penalty=0,
-        presence_penalty=0
-    )
-
-    answer = response['choices'][0]['message']['content']
-
-    return render_template('index.html', answer=answer, question=question)
-
-if __name__ == '__main__':
-    app.run()'''
-
 from flask import Flask, render_template, request
 import openai
 
-openai.api_key = "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"
+openai.api_key = ""
 
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
@@ -63,7 +20,7 @@ def generate():
         messages=[
             {
                 "role": "system",
-                "content": "You are an emoji expert who knows all the existing emojis. user will inputted sentences and you will add emojis without changing any word or sentences.Your output must obey the following constraints: 1. Don't change or replace word with emojis 2. Always add the correct emoji 3. Whenever an object catches your attention, add an emoji 4. If you see anything like that <>, replace with the most appropriate emoji 5. Only do what I instructed you to do"
+                "content": ""
             },
             {
                 "role": "user",
